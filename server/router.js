87be05143dd18c6ec624d30cc18abf14362de08f
eza5640 <eza5640@g.rit.edu>
2019-11-06 11:11:15 -0500

    finished domomaker d

--- server/router.js
+++ server/router.js
@@ -3,7 +3,8 @@ const mid = require('./middleware')
 const router = (app) => {
     app.get('/login', mid.requiresSecure, mid.requiresLogout, controllers.Account.loginPage);
     app.post('/login', mid.requiresSecure, mid.requiresLogout, controllers.Account.login);
-    app.get('/signup', mid.requiresSecure, controllers.Account.getToken);
+    app.get('/getToken', mid.requiresSecure, controllers.Account.getToken);
+    app.get('/getDomos', mid.requiresLogin, controllers.Domo.getDomos);
     app.post('/signup', mid.requiresSecure, mid.requiresLogout, controllers.Account.signup);
     app.get('/logout', mid.requiresLogin, controllers.Account.logout);
     app.get('/maker', mid.requiresLogin, controllers.Domo.makerPage);
