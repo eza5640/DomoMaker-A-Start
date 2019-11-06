447b4a8d5c9a8ff576d11a024d58800a0a27a037
eza5640 <eza5640@g.rit.edu>
2019-11-06 11:14:14 -0500

    domomaker d done

--- server/controllers/Account.js
+++ server/controllers/Account.js
@@ -3,85 +3,83 @@ const models = require('../models');
 const Account = models.Account;
 
 const loginPage = (req, res) => {
-    res.render('login', { csrfToken: req.csrfToken() });
-}
+  res.render('login', { csrfToken: req.csrfToken() });
+};
 
 const logout = (req, res) => {
-    req.session.destroy();
-    res.redirect('/');
-}
+  req.session.destroy();
+  res.redirect('/');
+};
 
 const login = (request, response) => {
-    const req = request;
-    const res = response;
+  const req = request;
+  const res = response;
 
-    const username = `${req.body.username}`;
-    const password = `${req.body.pass}`;
+  const username = `${req.body.username}`;
+  const password = `${req.body.pass}`;
 
-    if(!username || !password)
-    {
-        return res.status(400).json({ error: "All fields required" });
+  if (!username || !password) {
+    return res.status(400).json({ error: 'All fields required' });
+  }
+
+  return Account.AccountModel.authenticate(username, password, (err, account) => {
+    if (err || !account) {
+      return res.status(401).json({ error: 'Wrong username / password' });
     }
 
-    return Account.AccountModel.authenticate(username, password, (err, account) =>{
-        if (err || !account) {
-            return res.status(401).json({ error: 'Wrong username / password' });
-        }
+    req.session.account = Account.AccountModel.toAPI(account);
 
-        req.session.account = Account.AccountModel.toAPI(account);
-
-        return res.json({ redirect: '/maker' });
-    });
-}
+    return res.json({ redirect: '/maker' });
+  });
+};
 
 const signup = (request, response) => {
-    const req = request;
-    const res = response;
+  const req = request;
+  const res = response;
 
-    req.body.username = `${req.body.username}`;
-    req.body.pass = `${req.body.pass}`;
-    req.body.pass2 = `${req.body.pass2}`;
+  req.body.username = `${req.body.username}`;
+  req.body.pass = `${req.body.pass}`;
+  req.body.pass2 = `${req.body.pass2}`;
 
-    if(!req.body.username || !req.body.pass || !req.body.pass2) {
-        return res.status(400).json({ error: 'All fields required' });
-    }
-    if(req.body.pass !== req.body.pass2)
-    {
-        return res.status(400).json({ error: 'Passwords must match' });
-    }
+  if (!req.body.username || !req.body.pass || !req.body.pass2) {
+    return res.status(400).json({ error: 'All fields required' });
+  }
+  if (req.body.pass !== req.body.pass2) {
+    return res.status(400).json({ error: 'Passwords must match' });
+  }
 
-    return Account.AccountModel.generateHash(req.body.pass, (salt, hash) =>{
-        const accountData = {
-            username: req.body.username,
-            salt,
-            password: hash,
-        };
-        const newAccount = new Account.AccountModel(accountData);
-        const savePromise = newAccount.save();
-        savePromise.then(() => {
-            req.session.account = Account.AccountModel.toAPI(newAccount);
-            return res.json({ redirect: '/maker' });
-        });
-        savePromise.catch((err) =>{
-            console.log(err);
-
-            if(err.code === 11000) {
-                return res.status(400).json({ error: "Username already in use"});
-            }
-
-            return res.status(400).json({ error: "An error occurred" });
-        });
+  return Account.AccountModel.generateHash(req.body.pass, (salt, hash) => {
+    const accountData = {
+      username: req.body.username,
+      salt,
+      password: hash,
+    };
+    const newAccount = new Account.AccountModel(accountData);
+    const savePromise = newAccount.save();
+    savePromise.then(() => {
+      req.session.account = Account.AccountModel.toAPI(newAccount);
+      return res.json({ redirect: '/maker' });
     });
-}
+    savePromise.catch((err) => {
+      console.log(err);
+
+      if (err.code === 11000) {
+        return res.status(400).json({ error: 'Username already in use' });
+      }
+
+      return res.status(400).json({ error: 'An error occurred' });
+    });
+  });
+};
 
 const getToken = (request, response) => {
-    const req = request;
-    const res = response;
-    const csrfJSON = {
-        csrfToken: req.csrfToken(),
-    };
+  const req = request;
+  const res = response;
+  const csrfJSON = {
+    csrfToken: req.csrfToken(),
+  };
 
-    res.json(csrfJSON);
+  res.json(csrfJSON);
 };
 
 module.exports.loginPage = loginPage;
--- server/controllers/Domo.js
+++ server/controllers/Domo.js
@@ -3,56 +3,55 @@ const models = require('../models');
 const Domo = models.Domo;
 
 const makerPage = (req, res) => {
-    Domo.DomoModel.findByOwner(req.session.account._id, (err, docs) => {
-        if(err) {
-            console.log(err);
-            return res.status(400).json({ error: "An error occurred" });
-        }
-        return res.render('app', { csrfToken: req.csrfToken(), domos: docs });
-    });  
-}
+  Domo.DomoModel.findByOwner(req.session.account._id, (err, docs) => {
+    if (err) {
+      console.log(err);
+      return res.status(400).json({ error: 'An error occurred' });
+    }
+    return res.render('app', { csrfToken: req.csrfToken(), domos: docs });
+  });
+};
 
-const makeDomo = (req, res) =>{
-    if(!req.body.name || !req.body.age) {
-        return res.status(400).json({ error: "Name and age required" });
+const makeDomo = (req, res) => {
+  if (!req.body.name || !req.body.age) {
+    return res.status(400).json({ error: 'Name and age required' });
+  }
+
+  const domoData = {
+    name: req.body.name,
+    age: req.body.age,
+    owner: req.session.account._id,
+  };
+
+  const newDomo = new Domo.DomoModel(domoData);
+
+  const domoPromise = newDomo.save();
+  domoPromise.then(() => res.json({ redirect: '/maker' }));
+  domoPromise.catch((err) => {
+    console.log(err);
+    if (err.code === 11000) {
+      return res.status(400).json({ error: 'Domo already exists' });
     }
 
-    const domoData = {
-        name: req.body.name,
-        age: req.body.age,
-        owner: req.session.account._id,
-    };
+    return res.status(400).json({ error: 'An error occurred' });
+  });
 
-    const newDomo = new Domo.DomoModel(domoData);
-
-    const domoPromise = newDomo.save();
-    domoPromise.then(() => res.json({ redirect: '/maker' }));
-    domoPromise.catch((err) => {
-        console.log(err);
-        if(err.code === 11000)
-        {
-            return res.status(400).json({ error: "Domo already exists" });
-        }
-
-        return res.status(400).json({ error: "An error occurred" });
-    });
-
-    return domoPromise;
+  return domoPromise;
 };
 
 const getDomos = (request, response) => {
-    const req = request;
-    const res = response;
+  const req = request;
+  const res = response;
 
-    return Domo.DomoModel.findByOwner(req.session.account._id, (err, docs) => {
-        if(err) {
-            console.log(err);
-            return res.status(400).json({ error: 'An error occurred' });
-        }
+  return Domo.DomoModel.findByOwner(req.session.account._id, (err, docs) => {
+    if (err) {
+      console.log(err);
+      return res.status(400).json({ error: 'An error occurred' });
+    }
 
-        return res.json({ domos: docs });
-    });
-}
+    return res.json({ domos: docs });
+  });
+};
 
 module.exports.makerPage = makerPage;
 module.exports.getDomos = getDomos;
